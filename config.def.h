8bd456e2c4ff54258524c3992d1cd29d673cabcc
LorenzoMioso <lorenzo.mioso@gmail.com>
2020-12-29 05:07:37 +0100

    automatic commit

--- config.def.h
+++ config.def.h
@@ -101,6 +101,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
     { MODKEY,                       XK_Right,  view_adjacent,  {.i = +1 } },
 	{ MODKEY,                       XK_Left,   view_adjacent,  {.i = -1 } },
+	{ MODKEY,                       XK_c,      spawn,          SHCMD("=")},
 	{ MODKEY,                       XK_a,      spawn,          SHCMD("connectAukey")},
 	{ MODKEY|ShiftMask,             XK_a,      spawn,          SHCMD("disconnectAukey")},
 	{ MODKEY|ShiftMask,             XK_f,      spawn,          SHCMD("st ranger")},
