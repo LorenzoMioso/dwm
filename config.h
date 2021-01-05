f0ec3695e4c20ef1cc4e3ca26f3475c91b76be63
LorenzoMioso <lorenzo.mioso@gmail.com>
2021-01-06 00:47:17 +0100

    automatic commit

--- config.h
+++ config.h
@@ -6,8 +6,10 @@ static const int gappx              = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:pixelsize=14:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Hack Nerd Font:pixelsize=14:antialias=true:autohint=true" ;
+static const char *fonts[]          = { "heck:pixelsize=14:antialias=true:autohint=true" };
+static const char dmenufont[]       = "hack:pixelsize=14:antialias=true:autohint=true" ;
+/* static const char *fonts[]          = { "Hack Nerd Font:pixelsize=14:antialias=true:autohint=true" }; */
+/* static const char dmenufont[]       = "Hack Nerd Font:pixelsize=14:antialias=true:autohint=true" ; */
 //static const char *fonts[]          = { "monospace:size=10" };
 //static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#282828";
